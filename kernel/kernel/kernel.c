87a4d4bc18396a69fc2c795f301d8a1209cab9cf
Maor Winter <winter.maor@gmail.com>
2018-04-11 20:48:33 +0300

    Rebuild vga driver

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,6 +2,18 @@
 
 void kmain()
 {
-	terminal_initialize();
+	terminal_putchar(0, 'H', 1, 13);
+	terminal_putchar(1, 'e', 2, 12);
+	terminal_putchar(2, 'l', 3, 11);
+	terminal_putchar(3, 'l', 4, 10);
+	terminal_putchar(4, 'o', 5, 9);
+	terminal_putchar(5, ' ', 6, 8);
+	terminal_putchar(6, 'K', 14, 7);
+	terminal_putchar(7, 'e', 8, 6);
+	terminal_putchar(8, 'r', 9, 5);
+	terminal_putchar(9, 'n', 10, 4);
+	terminal_putchar(10, 'e', 11, 3);
+	terminal_putchar(11, 'l', 12, 2);
+	terminal_putchar(12, '!', 13, 1);
 	//printf("Hello, kernel World!\n");
 }
