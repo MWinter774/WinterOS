f15b8ae7dbadf6b7a1e71813106332b1cfc8779d
MaorWinter <winter.maor@gmail.com>
2018-04-12 14:33:17 +0300

    Small fixes

--- kernel/arch/i386/vga_driver.c
+++ kernel/arch/i386/vga_driver.c
@@ -7,35 +7,35 @@
 
 static volatile uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
 
-static unsigned char terminal_row = 0;
-static unsigned char terminal_column = 0;
+static unsigned char terminalRow = 0;
+static unsigned char terminalColumn = 0;
 
 void terminal_putchar(char c, char foregroundColor, char backgroundColor)
 {
 	switch(c)
 	{
 		case '\n': //if the char is the newline character
-			terminal_column = 0;
-			terminal_row++;
+			terminalColumn = 0;
+			terminalRow++;
 			break;
 		default: //if the char is a regular char
 			/* Writes the char to the vga memory address */
-			VGA_MEMORY[(VGA_COLS * terminal_row) + terminal_column] = ((backgroundColor & 0x0F) << 12) |
+			VGA_MEMORY[(VGA_COLS * terminalRow) + terminalColumn] = ((backgroundColor & 0x0F) << 12) |
 			 ((foregroundColor & 0x0F) << 8) | (c & 0xFF);
-			terminal_column++;
+			terminalColumn++;
 			break;
 	}
 	/* Moves the cursor */
-	if(terminal_column >= VGA_COLS)
+	if(terminalColumn >= VGA_COLS)
 	{
-		terminal_column = 0;
-		terminal_row++;
+		terminalColumn = 0;
+		terminalRow++;
 	}
-	if(terminal_row >= VGA_ROWS)
+	if(terminalRow >= VGA_ROWS)
 	{
 		//TODO: Scroll the screen
-		terminal_column = 0;
-		terminal_row = 0;
+		terminalColumn = 0;
+		terminalRow = 0;
 	}
 }
 
@@ -44,27 +44,27 @@ void terminal_putchar_default(char c)
 	switch(c)
 	{
 		case '\n': //if the char is the newline character
-			terminal_column = 0;
-			terminal_row++;
+			terminalColumn = 0;
+			terminalRow++;
 			break;
 		default: //if the char is a regular char
 			/* Writes the char to the vga memory address */
-			VGA_MEMORY[(VGA_COLS * terminal_row) + terminal_column] = ((VGA_COLOR_BLACK & 0x0F) << 12) |
+			VGA_MEMORY[(VGA_COLS * terminalRow) + terminalColumn] = ((VGA_COLOR_BLACK & 0x0F) << 12) |
 			 ((VGA_COLOR_LIGHT_GREY & 0x0F) << 8) | (c & 0xFF);
-			terminal_column++;
+			terminalColumn++;
 			break;
 	}
 	/* Moves the cursor */
-	if(terminal_column >= VGA_COLS)
+	if(terminalColumn >= VGA_COLS)
 	{
-		terminal_column = 0;
-		terminal_row++;
+		terminalColumn = 0;
+		terminalRow++;
 	}
-	if(terminal_row >= VGA_ROWS)
+	if(terminalRow >= VGA_ROWS)
 	{
 		//TODO: Scroll the screen
-		terminal_column = 0;
-		terminal_row = 0;
+		terminalColumn = 0;
+		terminalRow = 0;
 	}
 }
 
